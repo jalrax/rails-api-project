aba446385b69cdd307b7e4ab2222ac5e4c962b8d
jalrax <trymental@gmail.com>
2023-11-15 17:49:08 +0300

    Added more article tests

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController do
   describe '#index' do
-    it 'should returns a success responce' do
+    it 'should return a success responce' do
       get '/articles'
       expect(response).to have_http_status(:ok)
     end
@@ -10,21 +10,31 @@ RSpec.describe ArticlesController do
     it 'should return a proper JSON' do
       article = create :article
       get '/articles'
-      body = JSON.parse(response.body).deep_symbolize_keys
 
-      expect(body).to eq(
-        data: [
+      aggregate_failures do
+        expect(json_data.length).to eq(1)
+        expected = json_data.first
+
+        expect(expected[:id]).to eq(article.id.to_s)
+        expect(expected[:type]).to eq('article')
+        expect(expected[:attributes]).to eq(
           {
-            id: article.id.to_s,
-            type: 'article',
-            attributes: {
-              title: article.title,
-              content: article.content,
-              slug: article.slug
-            }
+            title: article.title,
+            content: article.content,
+            slug: article.slug
           }
-        ]
-      )
+        )
+      end
+    end
+
+    it 'should return articles in the correct order' do
+      recent_article = create(:article)
+      older_article = create(:article, created_at: 1.hour.ago)
+
+      get '/articles'
+      ids = json_data.map {_1[:id].to_i }
+
+      expect(ids).to eq([recent_article.id, older_article.id])
     end
   end
 end
--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe '/articles routes' do
-  it 'routes to articles#index' do
+  it 'should route to articles#index' do
     aggregate_failures do
       expect(get '/articles').to route_to('articles#index')
       expect(get '/articles?page[number]=3').to(
@@ -10,7 +10,7 @@ RSpec.describe '/articles routes' do
     end
   end
 
-  it 'routes to articles#show' do
+  it 'should route to articles#show' do
     expect(get('/articles/1')).to route_to('articles#show', id: '1')
   end
 end
